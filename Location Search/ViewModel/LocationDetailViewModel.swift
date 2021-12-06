51d0e7946520a5133a30cffaacadae65fc50e462
Gustavo Leguizamon <gustavo.leguizamon@contractor.brightinsight.com>
2021-12-06 18:23:24 -0300

    Added LocationDetailViewModel

--- Location Search/ViewModel/LocationDetailViewModel.swift
+++ Location Search/ViewModel/LocationDetailViewModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class LocationDetailViewModel: ObservableObject {
+    let location: LocationDTO
+    
+    private let apiClient: Client
+  
+    init(apiClient: Client = APIClient(), location: LocationDTO) {
+        self.location = location
+        self.apiClient = apiClient
+    }
+    
+    public func saveLocation(location: LocationDTO, completionHandler: @escaping(LocationDTO?)-> Void) {
+        apiClient.saveLocations(location: location) { location in
+            guard let location = location else {
+                completionHandler(nil)
+                return
+            }
+            completionHandler(location)
+        }
+    }
+}
