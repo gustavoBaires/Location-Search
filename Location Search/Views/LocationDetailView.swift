b2b8882bf30a17c92d33b1136add3993526986ec
Gustavo Leguizamon <gustavo.leguizamon@contractor.brightinsight.com>
2021-12-06 18:29:03 -0300

    Added LocationDetailView

--- Location Search/Views/LocationDetailView.swift
+++ Location Search/Views/LocationDetailView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct LocationDetailView: View {
+    
+    @ObservedObject var viewModel: LocationDetailViewModel
+    @State private var locationSaved: LocationDTO?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(viewModel.location.name)
+                .padding()
+            Text("Latitude: \(viewModel.location.latitude) Longitude: \(viewModel.location.latitude)")
+                .padding()
+            Spacer()
+            Button("Save Location") {
+                viewModel.saveLocation(location: viewModel.location) { location in
+                    guard let location = location else {
+                        return
+                    }
+                    locationSaved = location
+                }
+            }
+            .padding()
+            .alert(item: $locationSaved) { location in
+                Alert(title: Text("Important message"), message: Text("\(location.name) is saved."), dismissButton: .default(Text("Ok")))
+            }
+            Spacer()
+        }
     }
 }
 
 struct LocationDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationDetailView()
+        LocationDetailView(viewModel: LocationDetailViewModel(location: LocationDTO(id: "", name: "", latitude: "", longitude: "")))
     }
 }
